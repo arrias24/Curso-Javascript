f159e514f8e37dde96a4b61879384987b99f890a
arrias24 <andresarriasc@gmail.com>
2024-08-03 15:09:31 -0400

    feat: Introducion - Variables.

---
+++ Introduccion/variables.js
@@ -0,0 +1,24 @@
+//crear variables
+
+var marca, modelo, valor;
+let nombre,edad,telefono,genero,peso;
+const pi = 3.1415;
+
+//asignarles valores
+
+nombre = 'Andres';
+edad = 22;
+telefono = 3534534;
+genero = 'M';
+peso = 57.6;
+
+//concatenar variables
+
+let nombre_1 = 'Marco';
+let nombre_2 = 'Alvarez';
+let nombreCompleto = nombre_1 + nombre_2;
+
+//reedefinir tipo de dato de la variable
+
+let variable = 'texto';
+variable = 1;
