c13675c2397d0713e8adf5bba685161fcdad172e
arrias24 <andresarriasc@gmail.com>
2024-08-03 16:05:56 -0400

    Introduccion - Tipos de Datos.

---
+++ Introduccion/tiposDatos.js
@@ -0,0 +1,39 @@
+//strings
+
+let texto = 'aprendiendo javascript.';
+let texto_2 = 'Los robots tienen "sentimientos." ';
+
+// int, float
+
+let numero_entero = 15;
+let numero_decimal = 7.67;
+
+//bool
+
+let usuarioConectado = true;
+let mayorQue = 5 > 3;
+
+//Arrays
+
+let arreglo = ['Andres', 56.76, 22, {propiedad: 'valor'}, [1,2,3]];
+
+//Objetos
+
+const persona = {
+    nombre: 'Andres',
+    apellido: 'Arrias',
+    edad: 22,
+    carro:{
+        marca: 'Toyota',
+        color: 'verde'
+    }
+}
+
+console.log(persona);
+
+//Funciones
+
+function imprimirTexto()
+{
+    console.log('Esto es una funcion');
+};
