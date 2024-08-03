c0692223c88576a90e8f06f109d13be9cca80d40
arrias24 <andresarriasc@gmail.com>
2024-08-03 17:18:03 -0400

    Introduccion - Arreglos.

---
+++ Introduccion/arreglos.js
@@ -0,0 +1,11 @@
+const arreglo = ['Andres', 'Arrias', 57.8, {propiedad: 'valor'}, [1,2,3]];
+
+let colores = [];
+
+colores[0] = 'Azul';
+colores[1] = 'Verde';
+colores[2] = 'Rojo';
+colores[3] = 'Morado';
+
+colores.push('Blanco');
+console.log(colores + ' en total son: ' + colores.length + ' colores.');
