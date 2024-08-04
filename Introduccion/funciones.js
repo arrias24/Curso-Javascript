f73fdcf8bfae241f29f2260a93f509f436ae1fa2
arrias24 <andresarriasc@gmail.com>
2024-08-04 17:50:10 -0400

    Introduccion - Funciones.

---
+++ Introduccion/funciones.js
@@ -0,0 +1,17 @@
+// Forma 1
+
+// function saludo(){
+//     console.log('Hola!');
+// }
+// saludo();
+
+// Forma 2
+
+// const saludo = function(){
+//     console.log('Hola desde varibale!');
+// }
+
+// Forma 3
+
+// const saludo = () => console.log('Hola!');
+// saludo();
