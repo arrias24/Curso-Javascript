42cf266aa65d3c293d930c90a876ed0ba55059ba
arrias24 <andresarriasc@gmail.com>
2024-09-03 17:08:55 -0400

    Introduccion - Ciclo For.

---
+++ Introduccion/cicloFor.js
@@ -0,0 +1,11 @@
+for (let numero = 0; numero <= 10; numero++)
+{
+    console.log(numero);
+}
+
+const nombres = ['Carlos', 'Alberto', 'Diego', 'Nestor', 'Arturo', 'Juan'];
+
+for (let i = 0; i < nombres.length; i++)
+{
+    console.log(nombres[i]);
+}
